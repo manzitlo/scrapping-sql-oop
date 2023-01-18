a1375c4b5faaca80ab2e8f2c6a4cd793823aa66c
Wenzhe Luo <hellolwz1120@gmail.com>
2023-01-18 17:15:37 -0500

    creating class of read and store database

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-connection = sqlite3.connect("data.db")
-
 
 class Tour:
     def scrape(self, url):
@@ -26,7 +24,7 @@ class Tour:
 
 
 class Email:
-    def send(message):
+    def send(self, message):
         host = "smtp.gmail.com"
         port = 465
 
@@ -42,23 +40,27 @@ class Email:
         print("Email was sent!")
 
 
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
+class Database:
 
+    def __init__(self):
+        self.connection = sqlite3.connect("data.db")
 
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
+
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
 
 
 if __name__ == "__main__":
@@ -69,9 +71,10 @@ if __name__ == "__main__":
         print(extracted)
 
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = Database()
+            row = database.read(extracted)
             if not row:
-                store(extracted)
+                database.store(extracted)
                 email = Email()
                 email.send(message="Hey, new event was found!")
         time.sleep(2)
