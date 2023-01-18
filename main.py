6d08be7c330462c2d352f46ef7d3950043c1beb7
Wenzhe Luo <hellolwz1120@gmail.com>
2023-01-18 15:07:20 -0500

    creating class

--- main.py
+++ main.py
@@ -19,27 +19,27 @@ class Tour:
         source = response.text
         return source
 
-
     def extract(self, source):
         extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
         value = extractor.extract(source)["tours"]
         return value
 
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
+class Email:
+    def send(message):
+        host = "smtp.gmail.com"
+        port = 465
 
-    username = "app8flask@gmail.com"
-    password = "qyciukmocfaiarse"
+        username = "app8flask@gmail.com"
+        password = "qyciukmocfaiarse"
 
-    receiver = "app8flask@gmail.com"
-    context = ssl.create_default_context()
+        receiver = "app8flask@gmail.com"
+        context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("Email was sent!")
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print("Email was sent!")
 
 
 def store(extracted):
@@ -63,13 +63,15 @@ def read(extracted):
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        tour = Tour()
+        scraped = tour.scrape(URL)
+        extracted = tour.extract(scraped)
         print(extracted)
 
         if extracted != "No upcoming tours":
             row = read(extracted)
             if not row:
                 store(extracted)
-                send_email(message="Hey, new event was found!")
+                email = Email()
+                email.send(message="Hey, new event was found!")
         time.sleep(2)
